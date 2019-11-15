740a5298b8c4dea9e20155aacfe90063e9a74bc0
Samuel Hulme <shulme33@gmail.com>
2019-11-14 22:23:27 -0700

    Fixing the layout for the about page

--- src/components/MenuAbout.jsx
+++ src/components/MenuAbout.jsx
@@ -4,7 +4,15 @@ import "../css/menu.css";
 const MenuAbout = () => {
   return (
     <div className="menu">
-      <div className="menu-area"></div>
+      <div className="menu-area">
+        <div className="menu-name"></div>
+        <div className="about-half">
+          <div className="about-info"></div>
+        </div>
+        <div className="about-half">
+          <div className="about-picture"></div>
+        </div>
+      </div>
     </div>
   );
 };
