23ee1b6a71b808ac4787cb93b961e738d8c5b9ef
Samuel Hulme <shulme33@gmail.com>
2019-11-24 23:02:04 -0700

    Added contact form

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,24 @@ import "../App.css";
 const Banner = () => {
   return (
     <div className="banner">
-      <h1 className="banner-title">SAM HULME</h1>
+      {/*<h1 className="banner-title">SAM HULME</h1>*/}
+      <div className="contact-area">
+        <p className="contact-title">Contact Me!</p>
+        <form
+          className="contact-form"
+          action="https://formspree.io/mzbzpgvk"
+          method="POST"
+        >
+          <label className="contact-label">Your email:</label>
+          <input className="contact-input" type="text" name="_replyto" />
+          <label className="contact-label">Leave me a message:</label>
+          <textarea className="contact-text-area" name="message"></textarea>
+
+          <button className="contact-submit-button" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/MenuAbout.jsx
+++ src/components/MenuAbout.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../css/menu.css";
+import "../css/menu-resume.css";
 
 //<p className="about-half-title">Where I Grew Up</p>
 //<p className="about-half-text">Fort Benton, Montana</p>
@@ -14,10 +15,15 @@ const MenuAbout = () => {
           <div className="menu-name-underline"></div>
         </div>
 
-        {/* Profile Section */}
-        <div className="about-half about-info">
-          <div className="about-half-info">
-            <p className="about-half-title">Hello There!</p>
+        <div className="resume-full education-full">
+          <p className="resume-full-title">Hello There!</p>
+          <div className="education-section">
+            <div className="education-picture">
+              <img
+                src={require("../images/profile-picture-2.jpg")}
+                alt="Profile Picture"
+              />
+            </div>
             <p className="about-half-text">
               Hi! My name is Sam Hulme and I hope that you like my website. The
               main purpose of this website is to provide me with a fun place to
@@ -27,81 +33,44 @@ const MenuAbout = () => {
             </p>
           </div>
         </div>
-        <div className="about-half">
-          <div
-            className="about-picture"
-            style={{ width: "320px", height: "396px" }}
-          >
-            <img
-              className="about-picture"
-              src={require("../images/profile-picture-2.jpg")}
-              alt="Profile Picture"
-              width="300"
-            />
-          </div>
-        </div>
 
-        {/* Hometown Section */}
-        <div className="about-half">
-          <div
-            className="about-picture"
-            style={{ width: "476px", height: "320px" }}
-          >
-            <img
-              className="about-picture"
-              src={require("../images/fort-benton.jpg")}
-              alt="Profile Picture"
-              height="300"
-            />
-          </div>
-        </div>
-        <div className="about-half about-info">
-          <div className="about-half-info">
-            <p className="about-half-title">Where I Grew Up</p>
+        <div className="resume-full education-full">
+          <p className="resume-full-title">My Hometown</p>
+          <div className="education-section">
+            <div className="education-picture">
+              <img
+                src={require("../images/fort-benton.jpg")}
+                alt="Profile Picture"
+              />
+            </div>
             <p className="about-half-text">Fort Benton, Montana</p>
           </div>
         </div>
 
-        {/* Favorite Movie Section */}
-        <div className="about-half about-info">
-          <div className="about-half-info">
-            <p className="about-half-title">My Favorite Movie</p>
+        <div className="resume-full education-full">
+          <p className="resume-full-title">Favorite Movie</p>
+          <div className="education-section">
+            <div className="education-picture">
+              <img
+                src={require("../images/warrior-movie.jpg")}
+                alt="Profile Picture"
+              />
+            </div>
             <p className="about-half-text">
               Warrior (2011)<br></br>Directed By: Gavin O'Connor
             </p>
           </div>
         </div>
-        <div className="about-half">
-          <div
-            className="about-picture"
-            style={{ width: "270px", height: "404px" }}
-          >
-            <img
-              className="about-picture"
-              src={require("../images/warrior-movie.jpg")}
-              alt="Profile Picture"
-              width="250"
-            />
-          </div>
-        </div>
 
-        {/* Favorite Superhero Section */}
-        <div className="about-half">
-          <div
-            className="about-picture"
-            style={{ width: "420px", height: "420px" }}
-          >
-            <img
-              className="about-picture"
-              src={require("../images/batman-funny.jpg")}
-              alt="Profile Picture"
-              width="400"
-            />
-          </div>
-        </div>
-        <div className="about-half about-info">
-          <div className="about-half-info">
-            <p className="about-half-title">My Favorite Superhero</p>
+        <div className="resume-full education-full">
+          <p className="resume-full-title">Favorite Superhero</p>
+          <div className="education-section">
+            <div className="education-picture">
+              <img
+                src={require("../images/batman-funny.jpg")}
+                alt="Favorite Superhero"
+              />
+            </div>
             <p className="about-half-text">Batman.....duhh</p>
           </div>
         </div>
