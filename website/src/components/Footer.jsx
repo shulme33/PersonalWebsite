606562a6400ffa9aed2bdda08a9ecd38d87ec5a0
shulme33 <shulme33@gmail.com>
2020-07-18 08:27:14 -0600

    Adding to footer

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -13,16 +13,20 @@ import "../css/css-global/animations.css";
 function Footer() {
   return (
     <div className="mp mp-fit mp-color-5">
-      <div className="imp foot-imp">
-        <a href="https://www.instagram.com/shulme33/">
-          <div className="foot-social-icon icon-insta anim-upscale"></div>
-        </a>
-        <a href="https://www.facebook.com/sam.hulme.7">
-          <div className="foot-social-icon icon-fb anim-upscale"></div>
-        </a>
-        <a href="https://github.com/shulme33">
-          <div className="foot-social-icon icon-git anim-upscale"></div>
-        </a>
+      <div className="imp ftr-imp">
+        <div className="ftr-section ftr-social">
+          <p className="ftr-title">Follow me on social media!</p>
+          <ul className="ftr-social-list">
+            <li className="ftr-social-item ftr-linkedin"></li>
+            <li className="ftr-social-item ftr-instagram"></li>
+            <li className="ftr-social-item ftr-facebook"></li>
+            <li className="ftr-social-item ftr-github"></li>
+          </ul>
+        </div>
+        <div className="ftr-section ftr-contact">
+          <p className="ftr-title">Contact Me</p>
+        </div>
+        <div className="ftr-section ftr-rating"></div>
       </div>
     </div>
   );
@@ -31,18 +35,14 @@ function Footer() {
 export default Footer;
 
 /*
-<div className="mp mp-tiny mp-color-5">
-      <div className="imp foot-imp">
-        <a href="https://www.instagram.com/shulme33/">
-          <div className="foot-social-icon icon-insta anim-upscale"></div>
+<a href="https://www.instagram.com/shulme33/">
+          <div className="ftr-social-icon icon-insta anim-upscale"></div>
         </a>
         <a href="https://www.facebook.com/sam.hulme.7">
-          <div className="foot-social-icon icon-fb anim-upscale"></div>
+          <div className="ftr-social-icon icon-fb anim-upscale"></div>
         </a>
         <a href="https://github.com/shulme33">
-          <div className="foot-social-icon icon-git anim-upscale"></div>
+          <div className="ftr-social-icon icon-git anim-upscale"></div>
         </a>
-      </div>
-    </div>
 
 */
