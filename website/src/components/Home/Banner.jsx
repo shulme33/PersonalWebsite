40bbded14bd41382955673a2075720803942e0c5
shulme33 <shulme33@gmail.com>
2020-07-19 11:46:54 -0600

    Updated banner layout

--- website/src/components/Home/Banner.jsx
+++ website/src/components/Home/Banner.jsx
@@ -10,16 +10,14 @@ import "../../css/home-page/banner.css";
 
 function Banner() {
   return (
-    <div className="mp mp-fit mp-color-3">
-      <div className="ban-imp">
+    <div className="mp mp-fit mp-color-3 ban-mp-img">
+      <div className="imp imp-flex ban-imp">
         <div className="ban-img"></div>
         <div className="ban-img-txt">
+          <h3 className="mp-txt-color-2">― Hi, I'm ―</h3>
           <h1 className="mp-txt-color-2 ban-main-title">
             SAMUEL <br></br>HULME
           </h1>
-          <p className="mp-txt-color-2">Software Developer</p>
-          <p className="mp-txt-color-2">Enjoyer of Cocoa Wheats</p>
-          <p className="mp-txt-color-2">Stupid But Not Dumb</p>
         </div>
       </div>
     </div>
@@ -27,3 +25,11 @@ function Banner() {
 }
 
 export default Banner;
+
+/*
+
+<p className="mp-txt-color-2">Software Developer</p>
+          <p className="mp-txt-color-2">Enjoyer of Cocoa Wheats</p>
+          <p className="mp-txt-color-2">Stupid But Not Dumb</p>
+
+*/
