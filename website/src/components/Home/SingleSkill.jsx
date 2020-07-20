e677d57bd8d3cabee6a1c3f21cef9fec704e08ad
shulme33 <shulme33@gmail.com>
2020-07-20 07:34:10 -0600

    Added half-star functionality to Skills section

--- website/src/components/Home/SingleSkill.jsx
+++ website/src/components/Home/SingleSkill.jsx
@@ -5,9 +5,17 @@ function SingleSkill({ skillName, skillLevel }) {
   function determineSkillStarLevel() {
     var starObject = [];
     var i = 0;
-    for (i; i < skillLevel; i++) {
+
+    for (i; i < Math.floor(skillLevel); i++) {
       var tempObject = <div className="star-filled"></div>;
       starObject.push(tempObject);
+    }
+
+    if (skillLevel % 1 != 0) {
+      //This is something like 3.5 or 4.5 stars instead of a whole 3 or 4
+      var tempObject = <div className="star-half-filled"></div>;
+      starObject.push(tempObject);
+      i++;
     }
 
     for (i; i < 5; i++) {
--- website/src/components/Home/SkillsSection.jsx
+++ website/src/components/Home/SkillsSection.jsx
@@ -11,24 +11,24 @@ function SkillsSection() {
           mainTitle="Yeah, I've Got Skills"
           subTitle="Technical Skill Set"
         />
-        <SingleSkill skillName="HTML 5" skillLevel={3} />
-        <SingleSkill skillName="CSS 3" skillLevel={3} />
-        <SingleSkill skillName="JavaScript" skillLevel={3} />
+        <SingleSkill skillName="HTML 5" skillLevel={3.5} />
+        <SingleSkill skillName="CSS 3" skillLevel={3.5} />
+        <SingleSkill skillName="JavaScript" skillLevel={3.5} />
         <SingleSkill skillName="React" skillLevel={3} />
-        <SingleSkill skillName="Node.js" skillLevel={2} />
+        <SingleSkill skillName="Node.js" skillLevel={2.5} />
         <SingleSkill skillName="Python" skillLevel={4} />
         <SingleSkill skillName="Java" skillLevel={4} />
         <SingleSkill skillName="VB.NET" skillLevel={4} />
-        <SingleSkill skillName="C Programming" skillLevel={2} />
+        <SingleSkill skillName="C Programming" skillLevel={3} />
         <SingleSkill skillName="SQL" skillLevel={3} />
         <div className="skills-divider"></div>
-        <SingleSkill skillName="Git/GitHub" skillLevel={3} />
+        <SingleSkill skillName="Git/GitHub" skillLevel={3.5} />
         <SingleSkill skillName="VS Code" skillLevel={4} />
         <SingleSkill skillName="Visual Studio" skillLevel={4} />
         <SingleSkill skillName="SSMS" skillLevel={3} />
         <SingleSkill skillName="Microsoft Office" skillLevel={5} />
-        <SingleSkill skillName="XAMPP" skillLevel={2} />
-        <SingleSkill skillName="Figma" skillLevel={2} />
+        <SingleSkill skillName="XAMPP" skillLevel={2.5} />
+        <SingleSkill skillName="Figma" skillLevel={2.5} />
         <SingleSkill skillName="ES6: Skyrim" skillLevel={5} />
       </div>
     </div>
