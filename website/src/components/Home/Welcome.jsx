acf827eadafd8df2efb442e2644b294753a0fc9e
shulme33 <shulme33@gmail.com>
2020-07-20 07:19:09 -0600

    Updated language on Welcome and Work Exp sections

--- website/src/components/Home/Welcome.jsx
+++ website/src/components/Home/Welcome.jsx
@@ -18,12 +18,17 @@ function Welcome() {
       <div className="imp imp-flex wel-imp">
         <div className="wel wel-msg">
           <h3 className="wel-section-hdr mp-txt-color-2">Welcome!</h3>
-          <p className="wel-section-txt mp-txt-color-2">
-            This is the first long line of the text that will display here! It
-            can honestly be pretty long.
+          <p className="wel-section-txt-1 mp-txt-color-2">
+            Hello! Its good to see you. My name is Sam Hulme and I am a software
+            developer with over 2 years of professional working experience. My
+            passion lies in both front-end and back-end web development. I am a
+            driven, outgoing, and passionate person who is easy to work with and
+            can be relied on to get stuff done. This website contains all of my
+            necessary credentials as well as any random personal projects that I
+            might be interested in developing.
           </p>
-          <p className="wel-section-txt mp-txt-color-2">
-            There will also be this second shorter line.
+          <p className="wel-section-txt-2 mp-txt-color-2">
+            Feel free to take a look around and let me know what you think.
           </p>
         </div>
       </div>
--- website/src/components/Home/WorkExSection.jsx
+++ website/src/components/Home/WorkExSection.jsx
@@ -15,34 +15,38 @@ function WorkExSection() {
           jobIsLast={false}
           jobPicture=" job-fast-llc"
           jobCompany="Fast Enterprises, LLC"
-          jobTitle="Implementation Consultant"
+          jobTitle={[
+            "Implementation Consultant",
+            <br></br>,
+            "June 2018 - Present",
+          ]}
           jobLocation="New Mexico & Montana"
-          jobDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-          quasi beatae fugit nulla molestiae rerum veniam eos numquam atque
-          optio odio maiores aut, minus tempora quia, aperiam perspiciatis
-          dolorem vel?"
+          jobDescription={[
+            "In New Mexico, I was the lead developer on the MVD's online customer portal. The highlight of my time there was the complete re-design of the vehicle registrations process which helped to streamline the task for over 35,000 MVD customers.",
+            <br></br>,
+            <br></br>,
+            "Now in Montana, my attention has been focused on interfaces with the Montana Department of Revenue. I have overseen the conversion of our older FTP/Interface system to an updated/modern design.",
+          ]}
         />
         <Job
           jobIsLast={false}
           jobPicture=" job-msu-1"
           jobCompany="MSU Bozeman College of Engineering"
-          jobTitle="Research Assistant / Web Developer"
+          jobTitle={[
+            "Research Assistant / Web Developer",
+            <br></br>,
+            "May 2016 - June 2018",
+          ]}
           jobLocation="Bozeman, Montana"
-          jobDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-          quasi beatae fugit nulla molestiae rerum veniam eos numquam atque
-          optio odio maiores aut, minus tempora quia, aperiam perspiciatis
-          dolorem vel?"
+          jobDescription="As lead developer of the project, I helped design and implement an interactive, web-based homework system that is currently used by electrical engineering students at MSU. The system includes a virtual breadboard that allows students to design test circuits and helps improve the understanding of core fundementals without the need of a physical lab space."
         />
         <Job
           jobIsLast={true}
           jobPicture=" job-msu-2"
           jobCompany="MSU Bozeman Computer Science Department"
-          jobTitle="Course Assistant"
+          jobTitle={["Course Assistant", <br></br>, "August 2016 - May 2018"]}
           jobLocation="Bozeman, Montana"
-          jobDescription="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-          quasi beatae fugit nulla molestiae rerum veniam eos numquam atque
-          optio odio maiores aut, minus tempora quia, aperiam perspiciatis
-          dolorem vel?"
+          jobDescription="I oversaw lab sessions and instructed students in the principles of HTML, CSS, Java, and C programming in conjuction with course materials. My other duties involved holding out-of-class study sessions and grading student work."
         />
       </div>
     </div>
