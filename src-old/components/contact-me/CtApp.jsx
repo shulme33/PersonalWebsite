8b32a40405272f6e5019311d832035a6355727f2
Samuel Hulme <shulme33@gmail.com>
2020-12-31 20:07:45 -0700

    Reverted to bd6c9d9

--- src-old/components/contact-me/CtApp.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class CtApp extends Component {
-  render() {
-    return <h1>Contact Me App</h1>;
-  }
-}
-
-export default CtApp;
---
+++ src/components/Home/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../../css/css-global/structure.css";
+import "../../css/home-page/banner.css";
+
+/*
+
+    The large banner image at the top of the home page
+
+*/
+
+function Banner() {
+  return (
+    <div className="mp mp-fit mp-color-3 ban-mp-img">
+      <div className="imp imp-flex ban-imp">
+        <div className="ban-img-txt">
+          <h3 className="mp-txt-color-2">― Hi, I'm ―</h3>
+          <h1 className="mp-txt-color-2 ban-main-title">
+            SAMUEL <br></br>HULME
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
