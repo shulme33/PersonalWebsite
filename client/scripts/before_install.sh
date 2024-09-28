345d9f65b326e5e26f6bcc42ad057354ff2aee79
shulme33 <shulme33@gmail.com>
2024-09-28 16:54:51 -0700

    Added sudo

--- client/scripts/before_install.sh
+++ client/scripts/before_install.sh
@@ -14,7 +14,7 @@ sudo service nginx stop
 mrd_dir=$(cat /opt/codedeploy-agent/deployment-root/deployment-instructions/24ce635d-cce9-454e-b2cb-49c3d64bfee2_most_recent_install | grep 24ce635d-cce9-454e-b2cb-49c3d64bfee2)
 
 # Make directory: personalwebsite
-mkdir -p /usr/share/nginx/html/personalwebsite
+sudo mkdir -p /usr/share/nginx/html/personalwebsite
 
 # Delete existing files in nginx directory
 sudo rm -rf /usr/share/nginx/html
