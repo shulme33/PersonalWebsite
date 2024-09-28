7a6b40fec79389dea828b12aec1314677eb075bb
shulme33 <shulme33@gmail.com>
2024-09-28 16:48:04 -0700

    Fixes to before_install.sh

--- client/scripts/before_install.sh
+++ client/scripts/before_install.sh
@@ -12,5 +12,13 @@ sudo service nginx stop
 #deployment_group="24ce635d-cce9-454e-b2cb-49c3d64bfee2"
 
 mrd_dir=$(cat /opt/codedeploy-agent/deployment-root/deployment-instructions/24ce635d-cce9-454e-b2cb-49c3d64bfee2_most_recent_install | grep 24ce635d-cce9-454e-b2cb-49c3d64bfee2)
+
+# Make directory: personalwebsite
+mkdir -p /usr/share/nginx/html/personalwebsite
+
+# Delete existing files in nginx directory
 sudo rm -rf /usr/share/nginx/html
-sudo cp -a $mrd_dir/deployment-archive/build/. /usr/share/nginx/html/
+sudo rm -rf /usr/share/nginx/html/personalwebsite
+
+# Copy files into nginx directory
+sudo cp -a $mrd_dir/deployment-archive/build/. /usr/share/nginx/html/personalwebsite
