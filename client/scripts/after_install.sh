4072d98cc4cbf134bc8c32c2111124291cd70b03
shulme33 <shulme33@gmail.com>
2024-09-28 20:00:19 -0700

    Point at samhulme.com

--- client/scripts/after_install.sh
+++ client/scripts/after_install.sh
@@ -2,7 +2,7 @@
 echo "Running after_install.sh"
 
 # Copy nginx redirect script into correct directory
-nginx_conf_file="mbs.samhulme.com.conf"
+nginx_conf_file="samhulme.com.conf"
 sudo cp /var/www/personalwebsite/nginx/$nginx_conf_file /etc/nginx/conf.d/$nginx_conf_file
 
 
