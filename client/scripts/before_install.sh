a0bd4af2877af1413f2f44322ca203f66bf64d2d
shulme33 <shulme33@gmail.com>
2024-09-28 19:17:30 -0700

    Trying personalwebsite again

--- client/scripts/before_install.sh
+++ client/scripts/before_install.sh
@@ -8,7 +8,7 @@ project_build_dir="24ce635d-cce9-454e-b2cb-49c3d64bfee2"  # Get from server
 most_recent_install_file="/opt/codedeploy-agent/deployment-root/deployment-instructions/"$project_build_dir"_most_recent_install"
 
 # Stop Nginx
-sudo service nginx stop
+#sudo service nginx stop
 
 # Copy nginx redirect script into correct directory
-sudo cp /var/www/personalwebsite/nginx/$nginx_conf_file /etc/nginx/conf.d/$nginx_conf_file
+#sudo cp /var/www/personalwebsite/nginx/$nginx_conf_file /etc/nginx/conf.d/$nginx_conf_file
