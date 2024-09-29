be39b81d4516661a62c20dd66aa5e1ac3fa6f63e
shulme33 <shulme33@gmail.com>
2024-09-28 17:08:58 -0700

    Misc fixes to appspec

--- client/scripts/before_install.sh
+++ client/scripts/before_install.sh
@@ -13,12 +13,11 @@ sudo service nginx stop
 
 mrd_dir=$(cat /opt/codedeploy-agent/deployment-root/deployment-instructions/24ce635d-cce9-454e-b2cb-49c3d64bfee2_most_recent_install | grep 24ce635d-cce9-454e-b2cb-49c3d64bfee2)
 
+# Delete existing files in nginx directory
+sudo rm -rf /usr/share/nginx/html/personalwebsite
+
 # Make directory: personalwebsite
 sudo mkdir -p /usr/share/nginx/html/personalwebsite
-
-# Delete existing files in nginx directory
-sudo rm -rf /usr/share/nginx/html
-sudo rm -rf /usr/share/nginx/html/personalwebsite
 
 # Copy files into nginx directory
 sudo cp -a $mrd_dir/deployment-archive/build/. /usr/share/nginx/html/personalwebsite
