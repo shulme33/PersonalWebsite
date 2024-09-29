a36a3478284a852ef92848c594b75505b2365c09
shulme33 <shulme33@gmail.com>
2024-09-28 17:56:34 -0700

    Cleanup on before_install.sh

--- client/scripts/before_install.sh
+++ client/scripts/before_install.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 echo "Stopping Nginx and removing old code"
 
+# Values
+file_dir="/var/www/personalwebsite"
+nginx_conf_file="mbs.samhulme.com.conf"
+project_build_dir="24ce635d-cce9-454e-b2cb-49c3d64bfee2"  # Get from server
+most_recent_install_file="/opt/codedeploy-agent/deployment-root/deployment-instructions/"$project_build_dir"_most_recent_install"
+
 # Stop Nginx
 sudo service nginx stop
 
-# Remove old code
-#cd /var/www
-#sudo rm -r PersonalWebsite_OLD
-#sudo mv PersonalWebsite PersonalWebsite_OLD
-
-#deployment_group="24ce635d-cce9-454e-b2cb-49c3d64bfee2"
-
-mrd_dir=$(cat /opt/codedeploy-agent/deployment-root/deployment-instructions/24ce635d-cce9-454e-b2cb-49c3d64bfee2_most_recent_install | grep 24ce635d-cce9-454e-b2cb-49c3d64bfee2)
+# Get directory of most recent build
+mrd_dir=$(cat $most_recent_install_file | grep $project_build_dir)
 
 # Delete existing files in nginx directory
-sudo rm -rf /var/www/personalwebsite
+sudo rm -rf $file_dir
+sudo rm -f /etc/nginx/conf.d/$nginx_conf_file
 
-# Make directory: personalwebsite
+# Make directory if doesn't exist: /var/www/personalwebsite
 sudo mkdir -p /var
 sudo mkdir -p /var/www
-sudo mkdir -p /var/www/personalwebsite
+sudo mkdir -p $file_dir
 
 # Copy files into nginx directory
-sudo cp -a $mrd_dir/deployment-archive/build/. /var/www/personalwebsite
+sudo cp -a $mrd_dir/deployment-archive/build/. $file_dir
 
 # Copy nginx redirect script into correct directory
-sudo cp $mrd_dir/deployment-archive/nginx/mbs.samhulme.com.conf /etc/nginx/conf.d/mbs.samhulme.com.conf
+sudo cp $mrd_dir/deployment-archive/nginx/$nginx_conf_file /etc/nginx/conf.d/$nginx_conf_file
