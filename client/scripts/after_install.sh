cea06a24f8ad0d76e069e3242468616aa69a42fb
shulme33 <shulme33@gmail.com>
2024-09-28 19:33:07 -0700

    Re-added nginx conf commands

--- client/scripts/after_install.sh
+++ client/scripts/after_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "No current steps for after_install.sh"
+echo "Running after_install.sh"
+
+# Copy nginx redirect script into correct directory
+nginx_conf_file="mbs.samhulme.com.conf"
+sudo cp /var/www/personalwebsite/nginx/$nginx_conf_file /etc/nginx/conf.d/$nginx_conf_file
 
 
--- client/scripts/before_install.sh
+++ client/scripts/before_install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 echo "Stopping Nginx and removing old code"
 
-# Values
-file_dir="/var/www/personalwebsite"
-nginx_conf_file="mbs.samhulme.com.conf"
-project_build_dir="24ce635d-cce9-454e-b2cb-49c3d64bfee2"  # Get from server
-most_recent_install_file="/opt/codedeploy-agent/deployment-root/deployment-instructions/"$project_build_dir"_most_recent_install"
-
 # Stop Nginx
-#sudo service nginx stop
+sudo service nginx stop
 
-# Copy nginx redirect script into correct directory
-#sudo cp /var/www/personalwebsite/nginx/$nginx_conf_file /etc/nginx/conf.d/$nginx_conf_file
+mkdir /var/www/personalwebsite
+sudo chown -R ec2-user:ec2-user /var/www/personalwebsite
